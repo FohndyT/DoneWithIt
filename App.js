bc6b6d6ed0e4b3c6ecbe54c0ab4a23cf71ca02b7
FohndyT <fohndytah@gmail.com>
2025-08-12 00:20:34 -0400

    Playing with images

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from "react-native";
+import Constants from "expo-constants";
 
 export default function App() {
+  const hanfdlePress = () => console.log("Text pressed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress={hanfdlePress}>
+        Coucou tout le monde !
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Heeeyyyyy")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +32,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
